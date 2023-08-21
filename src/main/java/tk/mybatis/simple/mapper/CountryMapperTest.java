55dfaac72137a0584ef20bd4998de4477d92ef20
jojodav <623125462@qq.com>
2023-08-21 08:13:22 +0800

    架构好 java.xml 和 mapper.java

--- src/main/java/tk/mybatis/simple/mapper/CountryMapperTest.java
+++ src/main/java/tk/mybatis/simple/mapper/CountryMapperTest.java
@@ -1,36 +1,19 @@
 package tk.mybatis.simple.mapper;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import tk.mybatis.simple.model.Country;
 
-import java.io.IOException;
-import java.io.Reader;
 import java.util.List;
 
-public class CountryMapperTest {
-    private static SqlSessionFactory sqlSessionFactory;
-
-    @BeforeClass
-    public static void init(){
-        try {
-            Reader reader = Resources.getResourceAsReader("mybatis-config.xml");
-            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
-        }catch(IOException ignore){
-            ignore.printStackTrace();
-        }
-
-    }
+public class CountryMapperTest extends BaseMapperTest {
 
     @Test
     public void testSelectAll(){
-        SqlSession sqlSession=sqlSessionFactory.openSession();
+        SqlSession sqlSession=getSqlSession();
         try{
-            List<Country> countryList=sqlSession.selectList("selectAll");
+            List<Country> countryList=sqlSession.selectList(
+                    "tk.mybatis.simple.mapper.countryMapper.selectAll");
             printCountryList(countryList);
         }finally {
             sqlSession.close() ;
@@ -40,8 +23,11 @@ public class CountryMapperTest {
     private void printCountryList(List<Country> countryList){
         for (Country country: countryList) {
              System.out.printf("%-4d%4s%4s\n",
-                     country.getId(),country.getCountryName(),country.getCountryCode());
+                     country.getId(),
+                     country.getCountryName(),
+                     country.getCountryCode());
 
         }
     }
+
 }
--- src/main/java/tk/mybatis/simple/mapper/UserMapper.java
+++ src/main/java/tk/mybatis/simple/mapper/UserMapper.java
@@ -1,4 +1,15 @@
 package tk.mybatis.simple.mapper;
 
+import tk.mybatis.simple.model.SysUser;
+
+import java.util.List;
+
 public interface UserMapper {
+    SysUser selectById(Long id);
+
+    /**
+     * 查询全部用户
+     * @return
+     */
+    List<SysUser> selectAll();
 }
